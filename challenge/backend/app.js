2ee53366f3c8b5cd56668cc74965216506be50b4
Abdala <nignanamed0@gmail.com>
2025-03-03 12:53:58 +0000

    commit 4

--- challenge/backend/app.js
+++ challenge/backend/app.js
@@ -10,7 +10,11 @@ connectDB().then(r => console.log(r));
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['https://challenge-kpfy.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // Augmenter la taille maximale du corps de la requête à 10 Mo
 app.use(express.json({ limit: '10mb' }));
