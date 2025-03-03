216d401e3a74300243744475e726675b16bd5d20
Abdala <nignanamed0@gmail.com>
2025-03-03 14:23:39 +0000

    commit 5

--- challenge/frontend/src/components/header/Header.jsx
+++ challenge/frontend/src/components/header/Header.jsx
@@ -5,13 +5,13 @@ import { Link } from 'react-router-dom';
 const Header = ({ onLogout, showLoginButton = true }) => {
     return (
         <div className="py-2 border-b-2 mb-3">
-            <header className="container m-auto w-full flex  justify-between items-center align-center h-[67px] bg-white">
-                <div className="flex items-center w-full sm:w-auto">
+            <header className="container m-auto w-full flex justify-between items-center align-center h-[67px] bg-white">
+                <div className="flex items-center">
                     <img src={Logo} alt="Kimbiz Logo" className="w-10 h-auto mr-3" />
                     <h1 className="text-2xl font-bold">Kimbiz</h1>
                 </div>
 
-                <div className="flex items-center gap-4 w-full sm:w-auto justify-between sm:justify-end mt-3 sm:mt-0">
+                <div className="flex items-center gap-4">
                     {/* Bouton "Se déconnecter" */}
                     {onLogout && (
                         <button
@@ -26,7 +26,7 @@ const Header = ({ onLogout, showLoginButton = true }) => {
                     {showLoginButton && (
                         <Link
                             to="/login"
-                            className="px-6 py-3 sm:px-6 sm:py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         >
                             Me connecter
                         </Link>
