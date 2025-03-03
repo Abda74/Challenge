f99d3b9941a7dc24ba93c1e2d668b13b8ae9f1ff
Abdala <nignanamed0@gmail.com>
2025-03-03 14:21:56 +0000

    commit 5

--- challenge/frontend/src/components/header/Header.jsx
+++ challenge/frontend/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Header = ({ onLogout, showLoginButton = true }) => {
     return (
         <div className="py-2 border-b-2 mb-3">
-            <header className="container m-auto w-full flex flex-wrap justify-between items-center align-center h-[67px] bg-white">
+            <header className="container m-auto w-full flex  justify-between items-center align-center h-[67px] bg-white">
                 <div className="flex items-center w-full sm:w-auto">
                     <img src={Logo} alt="Kimbiz Logo" className="w-10 h-auto mr-3" />
                     <h1 className="text-2xl font-bold">Kimbiz</h1>
@@ -26,7 +26,7 @@ const Header = ({ onLogout, showLoginButton = true }) => {
                     {showLoginButton && (
                         <Link
                             to="/login"
-                            className="px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            className="px-6 py-3 sm:px-6 sm:py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500"
                         >
                             Me connecter
                         </Link>
