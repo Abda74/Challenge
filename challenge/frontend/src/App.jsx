e9c7eeb54372957c4fd676de412423e559549211
Abdala <nignanamed0@gmail.com>
2025-03-03 13:52:40 +0000

    commit 5

--- challenge/frontend/src/App.jsx
+++ challenge/frontend/src/App.jsx
@@ -7,8 +7,8 @@ const App = () => {
     return (
         <div className="App">
             <BrowserRouter>
-                <AuthRoutes />
                 <MyRoutes />
+                <AuthRoutes />
             </BrowserRouter>
         </div>
     );
